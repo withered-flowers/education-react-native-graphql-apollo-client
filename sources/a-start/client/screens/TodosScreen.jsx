292c855a20a3c47554a753d96dc41de15750118e
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-02-29 09:23:28 +0700

    feat: bump up version client

--- sources/a-start/client/screens/TodosScreen.jsx
+++ sources/a-start/client/screens/TodosScreen.jsx
@@ -1,69 +1,73 @@
-import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import styles from "../styles";
 
 const todosDataInitial = [
-  {
-    id: "1",
-    name: "In veniam non porro aliquam repellendus quisquam quia minus.",
-    completed: false,
-  },
-  {
-    id: "2",
-    name: "Molestias corporis est voluptates commodi.",
-    completed: true,
-  },
-  {
-    id: "3",
-    name: "Provident reiciendis porro nisi ea iusto nesciunt totam nesciunt.",
-    completed: false,
-  },
+	{
+		id: "1",
+		name: "In veniam non porro aliquam repellendus quisquam quia minus.",
+		completed: false,
+	},
+	{
+		id: "2",
+		name: "Molestias corporis est voluptates commodi.",
+		completed: true,
+	},
+	{
+		id: "3",
+		name: "Provident reiciendis porro nisi ea iusto nesciunt totam nesciunt.",
+		completed: false,
+	},
 ];
 
 const TodosCard = ({ item }) => {
-  return (
-    <View style={styles.containerCard}>
-      <Text style={styles.containerCardHeader}>Todos Name:</Text>
-      <Text style={styles.containerCardContent}>{item.name}</Text>
-      <Text style={styles.containerCardHeader}>Todos Completed?</Text>
-      <Text style={styles.containerCardContent}>
-        {item.completed.toString()}
-      </Text>
-      <TouchableOpacity style={styles.button} onPress={buttonOnPressHandler}>
-        <Text style={styles.buttonText}>Detail</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	const buttonOnPressHandler = () => {
+		// TODO: Add implementation here !
+	};
+
+	return (
+		<View style={styles.containerCard}>
+			<Text style={styles.containerCardHeader}>Todos Name:</Text>
+			<Text style={styles.containerCardContent}>{item.name}</Text>
+			<Text style={styles.containerCardHeader}>Todos Completed?</Text>
+			<Text style={styles.containerCardContent}>
+				{item.completed.toString()}
+			</Text>
+			<TouchableOpacity style={styles.button} onPress={buttonOnPressHandler}>
+				<Text style={styles.buttonText}>Detail</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const TodosScreen = ({ navigation }) => {
-  const addTodosButtonOnPressHandler = () => {
-    console.log("Stack - TodosList - Add Todo Clicked");
-    navigation.navigate("TodosAdd");
-  };
+	const addTodosButtonOnPressHandler = () => {
+		console.log("Stack - TodosList - Add Todo Clicked");
+		navigation.navigate("TodosAdd");
+	};
 
-  return (
-    <>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={addTodosButtonOnPressHandler}
-        >
-          <Text style={styles.buttonText}>Add Todo</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.containerFlatList}>
-        <Text style={styles.textHeader}>List Todos</Text>
-        <FlatList
-          data={todosDataInitial}
-          horizontal={false}
-          keyExtractor={(todo) => todo.id}
-          numColumns={2}
-          renderItem={TodosCard}
-          style={styles.flatList}
-        ></FlatList>
-      </View>
-    </>
-  );
+	return (
+		<>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={addTodosButtonOnPressHandler}
+				>
+					<Text style={styles.buttonText}>Add Todo</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.containerFlatList}>
+				<Text style={styles.textHeader}>List Todos</Text>
+				<FlatList
+					data={todosDataInitial}
+					horizontal={false}
+					keyExtractor={(todo) => todo.id}
+					numColumns={2}
+					renderItem={TodosCard}
+					style={styles.flatList}
+				/>
+			</View>
+		</>
+	);
 };
 
 export default TodosScreen;
--- sources/b-end/client/screens/TodosScreen.jsx
+++ sources/b-end/client/screens/TodosScreen.jsx
@@ -1,7 +1,7 @@
-import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 
 // TODO: Import the Hooks needed to use the GQL (useQuery)
-import { useQuery, useLazyQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 // TODO: Import the GQL here
 import { GET_TODOS, GET_TODO_BY_ID } from "../queries";
 
@@ -27,69 +27,69 @@ import styles from "../styles";
 // ];
 
 const TodosCard = ({ item }) => {
-  const [dispatch, { data }] = useLazyQuery(GET_TODO_BY_ID);
+	const [dispatch, { data }] = useLazyQuery(GET_TODO_BY_ID);
 
-  const buttonOnPressHandler = () => {
-    console.log("Stack - TodosList - Card - Detail Clicked");
-    dispatch({ variables: { todoId: Number(item.id) } });
-  };
+	const buttonOnPressHandler = () => {
+		console.log("Stack - TodosList - Card - Detail Clicked");
+		dispatch({ variables: { todoId: Number(item.id) } });
+	};
 
-  return (
-    <View style={styles.containerCard}>
-      <Text style={styles.containerCardHeader}>Todos Name:</Text>
-      <Text style={styles.containerCardContent}>{item.name}</Text>
-      <Text style={styles.containerCardHeader}>Todos Completed?</Text>
-      <Text style={styles.containerCardContent}>
-        {item.completed.toString()}
-      </Text>
-      <Text>{JSON.stringify(data)}</Text>
-      <TouchableOpacity style={styles.button} onPress={buttonOnPressHandler}>
-        <Text style={styles.buttonText}>Detail</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		<View style={styles.containerCard}>
+			<Text style={styles.containerCardHeader}>Todos Name:</Text>
+			<Text style={styles.containerCardContent}>{item.name}</Text>
+			<Text style={styles.containerCardHeader}>Todos Completed?</Text>
+			<Text style={styles.containerCardContent}>
+				{item.completed.toString()}
+			</Text>
+			<Text>{JSON.stringify(data)}</Text>
+			<TouchableOpacity style={styles.button} onPress={buttonOnPressHandler}>
+				<Text style={styles.buttonText}>Detail</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const TodosScreen = ({ navigation }) => {
-  // TODO: Use the hooks here
-  // useQuery will return an Object with some props included:
-  // - data: the returned data from query
-  // - loading: boolean, state whether the data is still being loaded or not?
-  // - error: Object, whether an error existed or not
-  //    - error.message: String, error message
-  const { data, loading, error } = useQuery(GET_TODOS);
+	// TODO: Use the hooks here
+	// useQuery will return an Object with some props included:
+	// - data: the returned data from query
+	// - loading: boolean, state whether the data is still being loaded or not?
+	// - error: Object, whether an error existed or not
+	//    - error.message: String, error message
+	const { data, loading, error } = useQuery(GET_TODOS);
 
-  const addTodosButtonOnPressHandler = () => {
-    console.log("Stack - TodosList - Add Todo Clicked");
-    navigation.navigate("TodosAdd");
-  };
+	const addTodosButtonOnPressHandler = () => {
+		console.log("Stack - TodosList - Add Todo Clicked");
+		navigation.navigate("TodosAdd");
+	};
 
-  if (loading) return <Text>Loading ...</Text>;
-  if (error) return <Text>{error.message}</Text>;
+	if (loading) return <Text>Loading ...</Text>;
+	if (error) return <Text>{error.message}</Text>;
 
-  return (
-    <>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={addTodosButtonOnPressHandler}
-        >
-          <Text style={styles.buttonText}>Add Todo</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.containerFlatList}>
-        <Text style={styles.textHeader}>List Todos</Text>
-        <FlatList
-          data={data.todos}
-          horizontal={false}
-          keyExtractor={(todo) => todo.id}
-          numColumns={2}
-          renderItem={({ item }) => <TodosCard item={item} />}
-          style={styles.flatList}
-        ></FlatList>
-      </View>
-    </>
-  );
+	return (
+		<>
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={addTodosButtonOnPressHandler}
+				>
+					<Text style={styles.buttonText}>Add Todo</Text>
+				</TouchableOpacity>
+			</View>
+			<View style={styles.containerFlatList}>
+				<Text style={styles.textHeader}>List Todos</Text>
+				<FlatList
+					data={data.todos}
+					horizontal={false}
+					keyExtractor={(todo) => todo.id}
+					numColumns={2}
+					renderItem={({ item }) => <TodosCard item={item} />}
+					style={styles.flatList}
+				/>
+			</View>
+		</>
+	);
 };
 
 export default TodosScreen;
